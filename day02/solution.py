bad5e9ec055d71bee30cc90d5d8d0e1775f67f06
Jesse Webb <gweebz@gmail.com>
2016-12-10 17:28:50 -0600

    Solution for part two of day 2

--- day02/solution.py
+++ day02/solution.py
@@ -5,7 +5,15 @@ PART_ONE_KEYPAD = [
     ['7', '8', '9']
 ]
 
-_STARTING_POSITION = (1, 1)
+PART_TWO_KEYPAD = [
+    [None, None, '1', None, None],
+    [None, '2', '3', '4', None],
+    ['5', '6', '7', '8', '9'],
+    [None, 'A', 'B', 'C', None],
+    [None, None, 'D', None, None],
+]
+
+_STARTING_KEY = '5'
 
 
 _MOVEMENTS = {
@@ -17,31 +25,43 @@ _MOVEMENTS = {
 
 
 def calculate_keypad_code(instructions, keypad):
-    current_keypad_position = _STARTING_POSITION
+    current_keypad_position = find_position_of_key(keypad, _STARTING_KEY)
     result = ''
     for instruction_line in instructions.splitlines():
         for movement in instruction_line:
-            current_keypad_position = _determine_new_keypad_position(current_keypad_position, movement)
-        result += keypad[current_keypad_position[0]][current_keypad_position[1]]
+            current_keypad_position = _determine_new_keypad_position(keypad, current_keypad_position, movement)
+        result += _get_key_at_position(keypad, current_keypad_position)
     return result
 
 
-def _determine_new_keypad_position(current_position, movement):
+def find_position_of_key(keypad, key):
+    return next((i, row.index(key)) for i, row in enumerate(keypad) if key in row)
+
+
+def _determine_new_keypad_position(keypad, current_position, movement):
 
     def _adjust_position_index(index, change):
         new_index = index + change
-        if new_index < 0 or new_index > 2:
+        if new_index < 0 or new_index >= len(keypad):  # assumes the keypad is square
             return index
         return new_index
 
-    return tuple(_adjust_position_index(pos, change) for pos, change in zip(current_position, _MOVEMENTS[movement]))
+    new_position = tuple(_adjust_position_index(pos, change)
+                         for pos, change in zip(current_position, _MOVEMENTS[movement]))
+    return new_position if _get_key_at_position(keypad, new_position) else current_position
+
+
+def _get_key_at_position(keypad, position):
+    return keypad[position[0]][position[1]]
 
 
 def solve():
     with open('puzzle_input.txt') as puzzle_input_file:
         puzzle_input = puzzle_input_file.read()
     part1_answer = calculate_keypad_code(puzzle_input, PART_ONE_KEYPAD)
+    part2_answer = calculate_keypad_code(puzzle_input, PART_TWO_KEYPAD)
     print 'Part One Answer: {}'.format(part1_answer)
+    print 'Part Two Answer: {}'.format(part2_answer)
 
 
 if __name__ == '__main__':
--- day02/solution_tests.py
+++ day02/solution_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from solution import calculate_keypad_code, PART_ONE_KEYPAD
+from solution import calculate_keypad_code, PART_ONE_KEYPAD, PART_TWO_KEYPAD
 
 
 class SolutionPartOneTests(unittest.TestCase):
@@ -72,3 +72,39 @@ class SolutionPartOneTests(unittest.TestCase):
         instructions = 'ULL\nRRDDD\nLURDL\nUUUUD'
         result = calculate_keypad_code(instructions, self.keypad)
         self.assertEqual('1985', result)
+
+
+class SolutionPartTwoTests(unittest.TestCase):
+
+    def setUp(self):
+        self.keypad = PART_TWO_KEYPAD
+
+    def test_one_move_up(self):
+        instructions = 'U'
+        result = calculate_keypad_code(instructions, self.keypad)
+        self.assertEqual('5', result)
+
+    def test_one_move_right(self):
+        instructions = 'R'
+        result = calculate_keypad_code(instructions, self.keypad)
+        self.assertEqual('6', result)
+
+    def test_two_moves_down(self):
+        instructions = 'DD'
+        result = calculate_keypad_code(instructions, self.keypad)
+        self.assertEqual('5', result)
+
+    def test_four_moves_in_a_circle(self):
+        instructions = 'URDL'
+        result = calculate_keypad_code(instructions, self.keypad)
+        self.assertEqual('A', result)
+
+    def test_eight_moves_in_a_circle(self):
+        instructions = 'UURRDDLL'
+        result = calculate_keypad_code(instructions, self.keypad)
+        self.assertEqual('D', result)
+
+    def test_two_lines_of_instructions(self):
+        instructions = 'RDLUULD\nRRRUDDDLUUU'
+        result = calculate_keypad_code(instructions, self.keypad)
+        self.assertEqual('64', result)
