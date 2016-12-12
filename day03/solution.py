11ae56f1207fe00238d20a5cde6bf269a0872b5d
Jesse Webb <gweebz@gmail.com>
2016-12-11 19:43:27 -0600

    Solution for part two of day 3

--- day03/solution.py
+++ day03/solution.py
@@ -2,25 +2,36 @@
 from itertools import permutations
 
 
-def count_possible_triangles(triangles_input):
+def count_possible_triangles(triangles_input, vertically=False):
     count = 0
-    for triangle_input in triangles_input.splitlines():
-        triangle_sides = [int(side) for side in triangle_input.split()]
-        assert len(triangle_sides) == 3, "invalid input: {}".format(triangle_input)
-        if _is_possible_triangle(triangle_sides):
+    triangles = _split_triangles(triangles_input, vertically)
+    for triangle in triangles:
+        if _is_valid_triangle(triangle):
             count += 1
     return count
 
 
-def _is_possible_triangle(triangle_sides):
-    return all([p[0] + p[1] > p[2] for p in permutations(triangle_sides)])
+def _split_triangles(triangles_input, vertically):
+    matrix = [tuple(int(side) for side in line_of_input.split()) for line_of_input in triangles_input.splitlines()]
+    if vertically:
+        return [tuple(matrix[y+s][x] for s in range(0, 3))
+                for y in range(0, len(matrix), 3)
+                for x in range(0, len(matrix[0]))]
+    else:
+        return matrix
+
+
+def _is_valid_triangle(triangle):
+    return all([p[0] + p[1] > p[2] for p in permutations(triangle)])
 
 
 def solve():
     with open('puzzle_input.txt') as puzzle_input_file:
         puzzle_input = puzzle_input_file.read()
     part1_answer = count_possible_triangles(puzzle_input)
+    part2_answer = count_possible_triangles(puzzle_input, vertically=True)
     print 'Part One Answer: {}'.format(part1_answer)
+    print 'Part Two Answer: {}'.format(part2_answer)
 
 
 if __name__ == '__main__':
--- day03/solution_tests.py
+++ day03/solution_tests.py
@@ -23,3 +23,23 @@ class SolutionPartOneTests(unittest.TestCase):
         triangles_input = '5 10 25'
         result = count_possible_triangles(triangles_input)
         self.assertEqual(0, result)
+
+
+class SolutionPartTwoTests(unittest.TestCase):
+
+    def test_three_possible_triangle(self):
+        triangles_input = '3  5  8\n' \
+                          '4 12 15\n' \
+                          '5 13 17\n'
+        result = count_possible_triangles(triangles_input, vertically=True)
+        self.assertEqual(3, result)
+
+    def test_example_input(self):
+        triangles_input = '101 301 501\n' \
+                          '102 302 502\n' \
+                          '103 303 503\n' \
+                          '201 401 601\n' \
+                          '202 402 602\n' \
+                          '203 403 603\n'
+        result = count_possible_triangles(triangles_input, vertically=True)
+        self.assertEqual(6, result)
