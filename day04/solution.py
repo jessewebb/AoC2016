0f0d885fbc0806b211982c898168bd082dec8c22
Jesse Webb <gweebz@gmail.com>
2016-12-22 21:00:39 -0600

    Solution for part two of day 4

--- day04/solution.py
+++ day04/solution.py
@@ -25,12 +25,7 @@ class Room(object):
             elif x_count < y_count:
                 return 1
             else:  # x_count == y_count; break tie by sorting letters alphabetically
-                if x_letter < y_letter:
-                    return -1
-                elif x_letter > y_letter:
-                    return 1
-                else:
-                    return 0
+                return -1 if x_letter < y_letter else (1 if x_letter > y_letter else 0)
 
         encrypted_name_letters = [char for char in self.encrypted_name if char.isalpha()]
         letter_counter = Counter(encrypted_name_letters)
@@ -39,6 +34,27 @@ class Room(object):
         five_most_common_letters = [letter for letter, count in sorted_most_common_letters_and_counts][:5]
         checksum_letters = [letter for letter in self.checksum]
         return five_most_common_letters == checksum_letters
+
+    @property
+    def name(self):
+        return self._decrypted_name()
+
+    def _decrypted_name(self):
+        chars = 'abcdefghijklmnopqrstuvwxyz'
+        char_count = len(chars)
+        letter_shift = self.sector_id
+
+        def roll_letter_forward(letter):
+            if letter not in chars:
+                return letter
+            shifted_index = chars.index(letter) + letter_shift % char_count  # shift right
+            if shifted_index >= char_count:  # we have passed the end, shift left back into range
+                shifted_index -= char_count
+            return chars[shifted_index]
+
+        decrypted_name = ''.join(roll_letter_forward(letter) for letter in self.encrypted_name).replace('-', ' ')
+        return decrypted_name
+
 
     @classmethod
     def from_input_line(cls, room_input_line):
@@ -58,18 +74,29 @@ class Room(object):
                (self.__class__.__name__, self.encrypted_name, self.sector_id, self.checksum)
 
 
+def _parse_rooms_from_text(list_of_rooms_as_text):
+    return [Room.from_input_line(room_input_line) for room_input_line in list_of_rooms_as_text.splitlines()]
+
+
 def calculate_sum_of_real_room_sector_ids(list_of_rooms_as_text):
-    rooms = [Room.from_input_line(room_input_line) for room_input_line in list_of_rooms_as_text.splitlines()]
+    rooms = _parse_rooms_from_text(list_of_rooms_as_text)
     return sum(room.sector_id for room in rooms if room.is_real)
+
+
+def find_sector_id_of_real_room(list_of_rooms_as_text, room_name):
+    rooms = _parse_rooms_from_text(list_of_rooms_as_text)
+    real_rooms = (room for room in rooms if room.is_real)
+    north_pole_storage_rooms = (room.sector_id for room in real_rooms if room.name == room_name)
+    return next(north_pole_storage_rooms, None)
 
 
 def solve():
     with open('puzzle_input.txt') as puzzle_input_file:
         puzzle_input = puzzle_input_file.read()
     part1_answer = calculate_sum_of_real_room_sector_ids(puzzle_input)
-    # part2_answer = foo(puzzle_input)
+    part2_answer = find_sector_id_of_real_room(puzzle_input, 'northpole object storage')
     print 'Part One Answer: {}'.format(part1_answer)
-    # print 'Part Two Answer: {}'.format(part2_answer)
+    print 'Part Two Answer: {}'.format(part2_answer)
 
 
 if __name__ == '__main__':
--- day04/solution_tests.py
+++ day04/solution_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day04.solution import Room, calculate_sum_of_real_room_sector_ids
+from day04.solution import Room, calculate_sum_of_real_room_sector_ids, find_sector_id_of_real_room
 
 
 class RoomIsRealTests(unittest.TestCase):
@@ -20,6 +20,45 @@ class RoomIsRealTests(unittest.TestCase):
     def test_example4(self):
         room = Room('totally-real-room', 200, 'decoy')
         self.assertFalse(room.is_real)
+
+
+class RoomNameTests(unittest.TestCase):
+
+    def test_roll_a_by_1_to_b(self):
+        room = Room('a', 1, 'test')
+        self.assertEqual('b', room.name)
+
+    def test_roll_b_by_1_to_c(self):
+        room = Room('b', 1, 'test')
+        self.assertEqual('c', room.name)
+
+    def test_roll_z_by_1_to_a(self):
+        room = Room('z', 1, 'test')
+        self.assertEqual('a', room.name)
+
+    def test_roll_a_by_25_to_z(self):
+        room = Room('a', 25, 'test')
+        self.assertEqual('z', room.name)
+
+    def test_roll_a_by_26_to_a(self):
+        room = Room('a', 26, 'test')
+        self.assertEqual('a', room.name)
+
+    def test_roll_a_by_52_to_a(self):
+        room = Room('a', 52, 'test')
+        self.assertEqual('a', room.name)
+
+    def test_hyphens_to_spaces(self):
+        room = Room('-', 0, 'test')
+        self.assertEqual(' ', room.name)
+
+    def test_letters_and_hyphens(self):
+        room = Room('abc-def', 79, 'test')  # 79 rolls over 3 times and then adds one letter
+        self.assertEqual('bcd efg', room.name)
+
+    def test_example(self):
+        room = Room('qzmt-zixmtkozy-ivhz', 343, 'test')
+        self.assertEqual('very encrypted name', room.name)
 
 
 class RoomFromInputLineTests(unittest.TestCase):
@@ -59,3 +98,31 @@ class CalculateSumOfRealRoomSectorIdsTests(unittest.TestCase):
         expected_sum_of_real_room_sector_ids = 1514
         result = calculate_sum_of_real_room_sector_ids(list_of_rooms_as_text)
         self.assertEqual(result, expected_sum_of_real_room_sector_ids)
+
+
+class FindSectorIdOfRealRoomTests(unittest.TestCase):
+
+    def test_only_entry_in_list_decrypted(self):
+        list_of_rooms_as_text = 'abc-def-0[abcde]\n'
+        result = find_sector_id_of_real_room(list_of_rooms_as_text, 'abc def')
+        self.assertEqual(result, 0)
+
+    def test_only_entry_in_list_encrypted(self):
+        list_of_rooms_as_text = 'zab-cde-53[abcde]\n'
+        result = find_sector_id_of_real_room(list_of_rooms_as_text, 'abc def')
+        self.assertEqual(result, 53)
+
+    def test_not_in_the_list(self):
+        list_of_rooms_as_text = 'abc-def-0[abcde]\n'
+        result = find_sector_id_of_real_room(list_of_rooms_as_text, 'not in the list')
+        self.assertIsNone(result)
+
+    def test_northpole_in_between_example_rooms_from_part_one(self):
+        list_of_rooms_as_text = 'aaaaa-bbb-z-y-x-123[abxyz]\n' \
+                                'a-b-c-d-e-f-g-h-987[abcde]\n' \
+                                'northpole-object-storage-260[oetra]\n' \
+                                'not-a-real-room-404[oarel]\n' \
+                                'totally-real-room-200[decoy]\n'
+        result = find_sector_id_of_real_room(list_of_rooms_as_text, 'northpole object storage')
+        self.assertEqual(result, 260)
+
