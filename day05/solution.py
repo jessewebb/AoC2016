d604b6ba2872f54c06331b4813a98b5c322c5690
Jesse Webb <gweebz@gmail.com>
2016-12-27 17:13:36 -0600

    Solution for part two of day 5

--- day05/solution.py
+++ day05/solution.py
@@ -1,24 +1,8 @@
 
 import hashlib
-import time
 
 
 def generate_password(door_id, password_length):
-
-    def _gen_password_chars():
-        i = 0
-        num_chars_generated = 0
-        while num_chars_generated < password_length:
-            md5 = hashlib.md5(door_id + str(i)).hexdigest()
-            i += 1
-            if md5.startswith('00000'):
-                num_chars_generated += 1
-                yield md5[5]
-
-    return ''.join(_gen_password_chars())
-
-
-def generate_password2(door_id, password_length):
 
     index = 0
     password = ''
@@ -30,18 +14,30 @@ def generate_password2(door_id, password_length):
     return password
 
 
+def generate_password2(door_id, password_length):
+
+    index = 0
+    password = ''.join('_' for _ in range(0, password_length))
+    while '_' in password:
+        md5 = hashlib.md5(door_id + str(index)).hexdigest()
+        if md5.startswith('00000'):
+            try:
+                position = int(md5[5])
+                character = md5[6]
+                if password[position] == '_':
+                    password = password[:position] + character + password[position + 1:]
+            except (IndexError, ValueError):
+                pass  # ignore invalid positions
+        index += 1
+    return password
+
+
 def solve():
     door_id = 'uqwqemis'
-    a1_t0 = time.time()
-    part1_answer1 = generate_password(door_id, 8)
-    a1_t1 = time.time()
-    a2_t0 = time.time()
-    part1_answer2 = generate_password2(door_id, 8)
-    a2_t1 = time.time()
-    # part2_answer = foo(puzzle_input)
-    print 'Part One Answer1: {} ({})'.format(part1_answer1, str(a1_t1-a1_t0))
-    print 'Part One Answer2: {} ({})'.format(part1_answer2, str(a2_t1-a2_t0))
-    # print 'Part Two Answer: {}'.format(part2_answer)
+    part1_answer = generate_password(door_id, 8)
+    part2_answer = generate_password2(door_id, 8)
+    print 'Part One Answer: {}'.format(part1_answer)
+    print 'Part Two Answer: {}'.format(part2_answer)
 
 
 if __name__ == '__main__':
--- day05/solution_tests.py
+++ day05/solution_tests.py
@@ -11,5 +11,11 @@ class SolutionPartOneTests(unittest.TestCase):
         password = generate_password(door_id, 8)
         self.assertEqual(password, '18f47a30')
 
+
+class SolutionPartTwoTests(unittest.TestCase):
+
+    def test_example(self):
+        door_id = 'abc'
+
         password = generate_password2(door_id, 8)
-        self.assertEqual(password, '18f47a30')
+        self.assertEqual(password, '05ace8e3')
